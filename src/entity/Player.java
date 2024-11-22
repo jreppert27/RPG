cae28e6a96bd3a23053005b7d0a4549d2d9483e3
jackr <jreppert@ithaca.edu>
2024-11-21 23:02:45 -0500

    added sprite animation

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -2,6 +2,9 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.KeyHandler;
@@ -24,6 +27,23 @@ public class Player extends Entity {
         x = 100;
         y = 100;
         speed = 4;
+    }
+
+    public void getPlayerImage() {
+        try {
+
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/ZER0.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/ZER0.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/ZER0.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/ZER0.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/ZER0.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/ZER0.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/ZER0.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/ZER0.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
